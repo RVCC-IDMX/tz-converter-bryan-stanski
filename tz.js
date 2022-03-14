7fe57b2c748c3fe3005f71333a4fb90f020ac118
Maglen-gg <bryan.with.a.wi@gmail.com>
2022-03-13 23:55:22 -0400

    Updated to use yargs

--- tz.js
+++ tz.js
@@ -2,17 +2,25 @@ const moment = require('moment-timezone');
 
 moment.tz.setDefault('America/New_York');
 
+const form = 'dddd, MMMM Do YYYY, h: mm:ss a';
+
+const yargs = require('yargs');
+
 let targetTimezone;
 
-if (process.argv.length != 3) {
-  console.log('Usage: node <script-file> <timezone>');
-  process.exit(1);
+if (yargs.argv.format === true) {
+  targetTimezone = yargs.argv._[0];
+  console.log(
+    `It is currently ${moment
+      .tz(targetTimezone)
+      .format(form)} in ${targetTimezone}`
+  );
+} else if (yargs.argv.time === true) {
+  targetTimezone = yargs.argv._[0];
+  console.log(
+    `It is currently ${moment.tz(targetTimezone)} in ${targetTimezone}`
+  );
 } else {
-  targetTimezone = process.argv[2];
+  console.log('Usage: node <script-file> <timezone> <--time or --format>');
+  process.exit[1];
 }
-
-console.log(
-  `The time at the ${targetTimezone} timezone is ${moment()
-    .tz(targetTimezone)
-    .format()}`
-);
